bf443a4e7293caac3fd0e692de59e32bbd401577
werayootK <werayoot5800@gmail.com>
2024-02-12 15:23:30 +0700

    Test Edit

---
+++ demo_forward_edit/input/image1.controller.js
@@ -0,0 +1,29 @@
+
+/**
+ * @controllerName :["getProductImage","updateProductImage","deleteProductImage","createProductImage"]
+ * @className :["ImageController"]
+ */
+      
+const getProductImage = asyncHandler(async (req, res) => { 
+    if (condition) {
+        return res()
+    }
+})
+
+const updateProductImage = asyncHandler(async (req, res) => { 
+    if (condition) {
+        return res()
+    }
+})
+
+const deleteProductImage = asyncHandler(async (req, res) => { 
+    if (condition) {
+        return res()
+    }
+})
+
+const createProductImage = asyncHandler(async (req, res) => { 
+    if (condition) {
+        return res()
+    }
+})
